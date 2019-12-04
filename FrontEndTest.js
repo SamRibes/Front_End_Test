c3a2f1f553ff01b75a476c87bc89fae0ed00689b
Sam <samuel.c.ribeiro@outlook.com>
2019-12-04 01:04:56 +0000

    Finished flatten function

---
+++ FrontEndTest.js
@@ -0,0 +1,55 @@
+
+class TreeTraversal {
+
+    //flattenedArray is empty when first calling the flatten method
+    //As recursion happens, the array is filled with the nodes from the tree
+
+    // static flatten(jsonObject) {
+
+    //     return flatten(jsonObject, flattenedArray);
+    // }
+
+    static flatten(jsonObject) {
+        var flattenedArray = [];
+        var i;
+
+        for (i = 0; i < jsonObject.length; i++) {
+            if (jsonObject[i].children === undefined)
+            {
+                flattenedArray.push(jsonObject[i]);
+            }
+            else
+            {
+                let recursedArray = TreeTraversal.flatten(jsonObject[i].children)
+                flattenedArray = flattenedArray.concat(recursedArray);
+            }
+        }
+
+        return flattenedArray;
+    }
+
+    static getParent(childID, jsonObject) {
+
+    jsonObject.find(node => node.id == childID);
+
+        return flattenedArray.find(node => node.id == parentId);
+    }
+
+    static addNode(parent, newNode) {
+
+        return null;
+    }
+
+}
+
+//parsing the tree.json into a js object I can manipulate
+let fs = require("fs");
+var jsonString = fs.readFileSync("tree.json").toString('utf-8');
+let jsonObject = JSON.parse(jsonString);
+
+let flattenedTree = TreeTraversal.flatten(jsonObject);
+
+console.log(flattenedTree);
+
+console.log(TreeTraversal.getParent("697eae2f-40dd-445e-a0f0-a918f3a4d5c0", flattenedTree))
+
