58c609d5946576a82344865c8ff19cb7bfe18685
Sam <samuel.c.ribeiro@outlook.com>
2019-12-04 21:08:02 +0000

    Work on getParent

--- FrontEndTest.js
+++ FrontEndTest.js
@@ -4,23 +4,18 @@ class TreeTraversal {
     //flattenedArray is empty when first calling the flatten method
     //As recursion happens, the array is filled with the nodes from the tree
 
-    // static flatten(jsonObject) {
-
-    //     return flatten(jsonObject, flattenedArray);
-    // }
-
     static flatten(jsonObject) {
         var flattenedArray = [];
         var i;
 
         for (i = 0; i < jsonObject.length; i++) {
-            if (jsonObject[i].children === undefined)
-            {
+            if (jsonObject[i].children === undefined) {
                 flattenedArray.push(jsonObject[i]);
             }
-            else
-            {
-                let recursedArray = TreeTraversal.flatten(jsonObject[i].children)
+            else {
+                var recursedArray = TreeTraversal.flatten(jsonObject[i].children)
+                delete jsonObject[i].children;
+                flattenedArray.push(jsonObject[i]);
                 flattenedArray = flattenedArray.concat(recursedArray);
             }
         }
@@ -28,25 +23,27 @@ class TreeTraversal {
         return flattenedArray;
     }
 
-    static getParent(childID, jsonObject) {
-
+    static getParent(nodeID, jsonObject) {
         var i;
+        // console.log(jsonObject);
 
         for (i = 0; i < jsonObject.length; i++) {
-            if (jsonObject[i].children === undefined)
-            {
-                flattenedArray.push(jsonObject[i]);
+            if (jsonObject[i].id == nodeID) {
+                return true;
             }
-            else
-            {
-                let recursedArray = TreeTraversal.flatten(jsonObject[i].children)
-                flattenedArray = flattenedArray.concat(recursedArray);
+            else {
+                if (jsonObject.children !== undefined) {
+
+                    var nextLevelHasChild = TreeTraversal.getParent(nodeID, jsonObject[i].children);
+
+                    if (nextLevelHasChild == true) {
+                        return jsonObject[i].id;
+                    }
+                }
             }
         }
 
-        jsonObject.find(node => node.id == childID);
-
-        return flattenedArray.find(node => node.id == parentId);
+        return null;    //No matching node with that ID found
     }
 
     static addNode(parent, newNode) {
@@ -56,14 +53,15 @@ class TreeTraversal {
 
 }
 
-//parsing the tree.json into a js object I can manipulate
-let fs = require("fs");
+//parsing the tree.json into a js object I can use
+var fs = require("fs");
 var jsonString = fs.readFileSync("tree.json").toString('utf-8');
-let jsonObject = JSON.parse(jsonString);
+var jsonObject = JSON.parse(jsonString);
 
-let flattenedTree = TreeTraversal.flatten(jsonObject);
+var flattenedTree = TreeTraversal.flatten(jsonObject);
+var retreivedParent = TreeTraversal.getParent("697eae2f-40dd-445e-a0f0-a918f3a4d5c0", jsonObject);
 
 console.log(flattenedTree);
 
-console.log(TreeTraversal.getParent("697eae2f-40dd-445e-a0f0-a918f3a4d5c0", flattenedTree))
+// console.log(retreivedParent);
 
