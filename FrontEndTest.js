7837b29175bd105cf342fce1dc84a515b97eeddb
Sam <samuel.c.ribeiro@outlook.com>
2019-12-06 14:07:27 +0000

    Fixed bug

--- FrontEndTest.js
+++ FrontEndTest.js
@@ -27,19 +27,28 @@ class TreeTraversal {
         var i;
 
         for (i = 0; i < tree.length; i++) {
-
+            console.log("Loop: " + i);
+            console.log("On Node: " + tree[i].name);
             if (tree[i].id === nodeID) 
             {
+                console.log("ID HAS BEEN FOUND!!!");
                 //if the node we're looking for is found then we return true to break out of the recursion
                 return true;
             }
             else if (typeof tree[i].children != "undefined") 
             {
-                var nextLevelHasNodeID = TreeTraversal.getParent(nodeID, tree[i].children);
+                console.log("Node has children");
+                var responseFromNextLevel = TreeTraversal.getParent(nodeID, tree[i].children);
 
-                if (nextLevelHasNodeID == true) 
+                if ((responseFromNextLevel == true)) //This node is the parent to the node we're looking for
                 {
+                    console.log("Response from next level is true... returning this node");
                     return tree[i];
+                }
+                if(typeof responseFromNextLevel == "object") //This is the node we're taking back up the recursion chain
+                {
+                    console.log("Response from next level is the node to return... returning this node");
+                    return responseFromNextLevel;
                 }
             }
         }
@@ -83,17 +92,18 @@ var fs = require("fs");
 var jsonString = fs.readFileSync("tree.json").toString('utf-8');
 
 
+
 var tree = JSON.parse(jsonString);
 var flattenedTree = TreeTraversal.flatten(tree);
-fs.writeFile("TreeTrversal1.json", JSON.stringify(flattenedTree), function(err) {
+fs.writeFile("TreeTraversal1.json", JSON.stringify(flattenedTree), function(err) {
     if (err) {
         console.log(err);
     }
 });
 
 var tree = JSON.parse(jsonString);
-var retreivedParent = TreeTraversal.getParent("697eae2f-40dd-445e-a0f0-a918f3a4d5c0", tree);
-fs.writeFile("TreeTrversal2.json", JSON.stringify(retreivedParent), function(err) {
+var retreivedParent = TreeTraversal.getParent("697eae2f-40dd-445e-a0f0-111111111111", tree);
+fs.writeFile("TreeTraversal2.json", JSON.stringify(retreivedParent), function(err) {
     if (err) {
         console.log(err);
     }
@@ -105,8 +115,10 @@ var newNode = {
     id: "697eae2f-40dd-445e-a0f0-111111111111"
 }
 var treeWithAddedNode = TreeTraversal.addNode(tree, "c64ac3d7-5e88-40ed-8d6f-e7f10d21c5bc", newNode, 0);
-fs.writeFile("TreeTrversal3.json", JSON.stringify(treeWithAddedNode), function(err) {
+fs.writeFile("TreeTraversal3.json", JSON.stringify(treeWithAddedNode), function(err) {
     if (err) {
         console.log(err);
     }
 });
+
+console.log(retreivedParent);
